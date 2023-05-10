6f8f188e315fe12ac31c6c916cfaff4f0ad34897
MMentol <112599969+MMentol@users.noreply.github.com>
2023-05-11 04:43:12 +0800

    Added RegisterServlet, added web.xml servlet mapping, added code for logout servlet

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -1,1 +1,24 @@
-package java.controller;
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import nl.captcha.Captcha;
+import model.Crypto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginServlet extends HttpServlet {
+    
+}
