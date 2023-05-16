a82fb049ac527eb2ff1e16c24a541c2ddd26a83b
MMentol <112599969+MMentol@users.noreply.github.com>
2023-05-16 19:14:35 +0800

    shopinit edit

--- src/java/model/ShopInitializer.java
+++ src/java/model/ShopInitializer.java
@@ -1,19 +1,19 @@
 package model;
 
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class ShopInitializer {
     
     
-    public ArrayList<ShopItem> initStock(ResultSet rs){
-        ArrayList<ShopItem> stonk = new ArrayList();
+    public HashMap<String,ShopItem> initStock(ResultSet rs){
+        HashMap<String,ShopItem> stonk = new HashMap();
         
         try{
             //Loops until all items have been loaded.
             while(rs.next()){
-            stonk.add(new ShopItem(
+            stonk.put(rs.getString("STOCK_ID"),new ShopItem(
                     rs.getString("STOCK_ID"),
                     rs.getString("STOCK_NAME"),
                     Double.parseDouble(rs.getString("STOCK_PRICE")),
